89333ab4bce66e7bbd8ba8d2fb08f3f41510cbd2
dagon1234 <dagontunk1234@gmail.com>
2023-09-19 16:06:15 +0700

    structure microservice

---
+++ aword-microservice/src/test/java/com/aword/awordmicroservice/AwordMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aword.awordmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwordMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ director-microservice/src/test/java/com/director/directormicroservice/DirectorMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.director.directormicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectorMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
