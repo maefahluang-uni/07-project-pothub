375e528911fee6b14abc0a12419b7dfe74ac4c72
dagon1234 <dagontunk1234@gmail.com>
2023-09-24 18:31:10 +0700

    have error in Award controller and repository

--- filmingmovie-microservic/src/main/java/com/microservice/filmingmoviemicroservic/AwardRepository.java
+++ filmingmovie-microservic/src/main/java/com/microservice/filmingmoviemicroservic/AwardRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface AwardRepository extends CrudRepository<Award, Long> {
-    public List<Award> findByName(String name);
+    // public List<Award> findByName(String name);
 }
