4baf4e49971e9776558f9e86bda1811a5d4d05fd
Potterpurpp <118960067+Potterpurpp@users.noreply.github.com>
2023-10-02 23:28:27 +0700

    Fix: MovieController and Addmovie complete success

--- filmingmovie-microservic/src/main/java/com/microservice/filmingmoviemicroservic/MovieController.java
+++ filmingmovie-microservic/src/main/java/com/microservice/filmingmoviemicroservic/MovieController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class MovieController {
 
     @Autowired
